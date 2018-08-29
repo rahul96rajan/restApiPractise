660321a15b5f906562879d077f9e136e61678e05
rahul96rajan <rahulrajan@qainfotech.com>
2018-08-29 17:33:13 +0530

    Minor Updgrades

--- src/main/java/com/practise/client/RestClient.java
+++ src/main/java/com/practise/client/RestClient.java
@@ -17,14 +17,21 @@ public class RestClient {
 	
 	public void get(String url) throws ClientProtocolException, IOException {
 		
+		//Get Method
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		HttpGet httpget = new HttpGet(url);
 		CloseableHttpResponse closebaleHttpResponse = httpClient.execute(httpget);
+		
+		// Status Code
 		int statusCode = closebaleHttpResponse.getStatusLine().getStatusCode();
 		System.out.println("Status Code ----> " + statusCode);
+		
+		// JSon String
 		String responseString = EntityUtils.toString(closebaleHttpResponse.getEntity(),"UTF-8");
 		JSONObject responseJson = new JSONObject(responseString);
 		System.out.println("REsponse JSON --> " + responseJson);
+		
+		// Fetch Headers
 		Header[] headerArray = closebaleHttpResponse.getAllHeaders();
 		HashMap<String, String> allHeaders = new HashMap<String, String>();
 		for (Header header : headerArray) {
@@ -32,6 +39,17 @@ public class RestClient {
 		}
 		System.out.println("All Headers : \n "+ allHeaders);
 		
+		String jsonString = responseJson.toString();
+		String tmp = jsonString.substring(jsonString.indexOf("en\":")+5, jsonString.indexOf("\"}"));
+		System.out.println("\n\n ONLY TOKEN :\n " + tmp);
+		
 	}
+	
+	public void meth() {
+		
+	}
+	
+	
+	
 
 }
